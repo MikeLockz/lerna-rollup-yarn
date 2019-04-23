9ebd7761256835821377382ded7d2457247fea22
Mike Lock <michael.lockwitz@consensys.net>
2019-04-23 14:02:21 -0600

    Fixing yarn build command.

--- packages/_tooling/eslint.config.js
+++ packages/_tooling/eslint.config.js
@@ -1,18 +1,18 @@
 module.exports = {
-  parser: "babel-eslint",
-  extends: ["airbnb", "prettier", "prettier/react"],
-  plugins: ["react", "prettier"],
+  parser: 'babel-eslint',
+  extends: ['airbnb', 'prettier', 'prettier/react'],
+  plugins: ['react', 'prettier'],
   rules: {
-    "react/jsx-filename-extension": [
+    'react/jsx-filename-extension': [
       1,
       {
-        extensions: [".js", "jsx"]
-      }
+        extensions: ['.js', 'jsx'],
+      },
     ],
-    "prettier/prettier": "error",
-    "max-len": ["error", 80]
+    'prettier/prettier': 'error',
+    'max-len': ['error', 80],
   },
   env: {
-    browser: true
-  }
+    browser: true,
+  },
 };
